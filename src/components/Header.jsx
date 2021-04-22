f1dd8e8bcbf75f990ec1373f710559e554657ac6
robotrainer <asbu26@gmail.com>
2021-04-22 16:06:35 +0900

    social network 22.04.2021

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://cdn.pixabay.com/photo/2020/06/09/17/36/donut-5279363_960_720.png"></img>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://www.ejin.ru/wp-content/uploads/2019/05/kanada.jpg" />
+      </div>
+      <div>ava + description</div>
+      <div>
+        My Post
+        <div>New post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
