f76f06e6b180888f1ce0d3939cc532dd3ea578d4
robotrainer <asbu26@gmail.com>
2021-04-25 23:22:44 +0900

    add NavLink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import classes from "./Header.module.css";
 const Header = () => {
   return (
     <header className={classes.header}>
+      <h1 className={classes.h1}>MySocialNetwork</h1>
       <img src="https://cdn.pixabay.com/photo/2020/06/09/17/36/donut-5279363_960_720.png"></img>
     </header>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,34 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./Navbar.module.css";
 
 const Nav = () => {
   return (
     <nav className={classes.nav}>
       <div className={classes.item}>
-        <a href="/profile">Profile</a>
+        <NavLink to="/profile" activeClassName={classes.activeLink}>
+          Profile
+        </NavLink>
       </div>
       <div className={classes.item}>
-        <a href="/dialogs">Messages</a>
+        <NavLink to="/dialogs" activeClassName={classes.activeLink}>
+          Messages
+        </NavLink>
       </div>
       <div className={classes.item}>
-        <a>News</a>
+        <NavLink to="/news" activeClassName={classes.activeLink}>
+          News
+        </NavLink>
       </div>
       <div className={classes.item}>
-        <a>Settings</a>
+        <NavLink to="/music" activeClassName={classes.activeLink}>
+          Music
+        </NavLink>
+      </div>
+      <div className={classes.item}>
+        <NavLink to="settings" activeClassName={classes.activeLink}>
+          Settings
+        </NavLink>
       </div>
     </nav>
   );
